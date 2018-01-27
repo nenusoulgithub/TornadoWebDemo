2f8341d302dd9bacbad64ee6dafc99457feb7fbe
郭华 <hua.guo@tadu.com>
2018-01-27 16:32:09 +0800

    初始化模板项目

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 # -*- coding: UTF-8 -*-
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
 # 参数
 options = {
     "port": 9000
@@ -6,5 +10,7 @@ options = {
 
 # 配置
 settings = {
-    "debug": True
+    "debug": True,
+    "static_path": os.path.join(BASE_DIR, "static"),
+    "templetes_path": os.path.join(BASE_DIR, "templetes"),
 }
